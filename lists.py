8b826f6249e69d7707e3bbeecd4a66335c7a82c9
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-05-10 14:58:20 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -8,44 +8,37 @@ def remove_elements(x):
 		return x			
 	else:
 		return x
-
 def add_elements(x):
 	x.insert(0,"Pink")
 	x.insert(len(x), "Yellow")
 	return x
 #add_elements=['Red', 'Green', 'White', 'Black']
 #print(add_elements(add_elements))
-
 def is_empty(lista):
 	if lista==[]:
 		return True
 	else:
 		return False
-
-
 def check_lists(x,y):
 	if len(x)>2 and len(y)>2:
 		if x[2]==y[2]: 
-			return True
+			return "True"
 		else:
 			return False
 	else:
 		return False
-
-
 def list_of_lists(x):
 	if len(x)>2:
 		if len(x[0])>=1:
 			p1 = x[0][0:2]
 		else:
 			p1=[]
-
 		if len(x[1])>=2:
 			p2=x[1][1:4]
 		else:
 			p2=[]
 		if len(x[2])>=1:
 			p3=x[2][-2:]
-      		else:
+		else:
 			p3=[]
 		return [p1, p2, p3]
